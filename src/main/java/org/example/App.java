d02385202ec5c4b27c5fa147b8f826276bb9035f
Thach Vo <thachvo530@yahoo.com>
2021-09-11 17:22:03 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Thach Vo
+ */
+
+package org.example;
+
+public class App{
+
+    public static void main(String[] args) {
+
+        String quote = "These aren't the droids you're looking for.";
+
+        String speaker = "Obi-Wan Kenobi";
+
+        System.out.println("What is the quote? "+ quote +"\nWho said it? "+ speaker +"\n"+ speaker +" says, \"" + quote + "\"\n");
+    }
+}
